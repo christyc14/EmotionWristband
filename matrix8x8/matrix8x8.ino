81768d523ac642df5dc429846c2af7f2f3b0d2b5
christyc14 <56485161+christyc14@users.noreply.github.com>
2021-06-09 21:59:11 +0100

    matrix

---
+++ matrix8x8/matrix8x8.ino
@@ -0,0 +1,96 @@
+/*************************************************** 
+  This is a library for our I2C LED Backpacks
+
+  Designed specifically to work with the Adafruit LED Matrix backpacks 
+  ----> http://www.adafruit.com/products/872
+  ----> http://www.adafruit.com/products/871
+  ----> http://www.adafruit.com/products/870
+
+  These displays use I2C to communicate, 2 pins are required to 
+  interface. There are multiple selectable I2C addresses. For backpacks
+  with 2 Address Select pins: 0x70, 0x71, 0x72 or 0x73. For backpacks
+  with 3 Address Select pins: 0x70 thru 0x77
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+
+Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8x8 LED Matrix Test");
+  
+  matrix.begin(0x70);  // pass in the address
+}
+
+static const uint8_t PROGMEM
+  smile_bmp[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10100101,
+    B10011001,
+    B01000010,
+    B00111100 },
+  neutral_bmp[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10111101,
+    B10000001,
+    B01000010,
+    B00111100 },
+  frown_bmp[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10011001,
+    B10100101,
+    B01000010,
+    B00111100 };
+
+void loop() {
+  matrix.clear();
+  matrix.drawBitmap(0, 0, smile_bmp, 8, 8, LED_ON);
+  matrix.writeDisplay();
+  delay(500);
+
+  matrix.setBrightness(3);
+  matrix.setTextSize(1);
+  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+  matrix.setTextColor(LED_ON);
+  for (int8_t x=0; x>=-45; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("In 2 3 4");
+    matrix.writeDisplay();
+    delay(100);
+  }
+  for (int8_t x=9; x>=-90; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("Hold 2 3 4 5 6 7");
+    matrix.writeDisplay();
+    delay(100);
+  }
+  for (int8_t x=16; x>=-103; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("Out 2 3 4 5 6 7 8");
+    matrix.writeDisplay();
+    delay(100);
+  }
+  matrix.setRotation(0);
+}
